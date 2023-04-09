baf3a94c80f3f1a93ca16fc187accd0146db5f8f
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-04-09 10:22:31 +0900

    Update access.go

--- ent/schema/access.go
+++ ent/schema/access.go
@@ -5,6 +5,7 @@ import (
 
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
 )
 
 // Access holds the schema definition for the Access entity.
@@ -18,12 +19,21 @@ func (Access) Fields() []ent.Field {
 		// UUIDの場合は、別途考える
 		// ハッシュテーブルを使った方が良い場合もある
 		field.String("name").
+			MaxLen(25).
+			NotEmpty().
 			Default("unknown"),
 		field.Time("check_in").
 			Default(time.Now()),
 		field.Time("check_out").
 			Optional().
 			Nillable(),
+	}
+}
+
+// createdAt and updatedAt Method
+func (Access) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixin.Time{},
 	}
 }
 
