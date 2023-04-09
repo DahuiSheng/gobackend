463678e055f071dbbf573b43a9e466b57a9317ab
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-04-09 10:14:46 +0900

    Make schema at access.go

--- ent/schema/access.go
+++ ent/schema/access.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // Access holds the schema definition for the Access entity.
 type Access struct {
@@ -9,7 +14,17 @@ type Access struct {
 
 // Fields of the Access.
 func (Access) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		// UUIDの場合は、別途考える
+		// ハッシュテーブルを使った方が良い場合もある
+		field.String("name").
+			Default("unknown"),
+		field.Time("check_in").
+			Default(time.Now()),
+		field.Time("check_out").
+			Optional().
+			Nillable(),
+	}
 }
 
 // Edges of the Access.
