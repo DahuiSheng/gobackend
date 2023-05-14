737554c2136f49abc76dec8fb8cb5c49ffd05b61
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-05-14 15:09:29 +0900

    MAKE API

--- app/controllers/Enter/EnterController.go
+++ app/controllers/Enter/EnterController.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // よりStrictに定義する
 type EnterController struct{}
@@ -9,19 +13,31 @@ type EnterController struct{}
 func (EnterController) EnterPut(c *gin.Context) {
 
 	// :=は、代入と型定義を一緒に行なっている
-	json := EnterPOSTReqBody{}
+	json := EnterPUTReqBody{}
 
-	c.JSON(200, gin.H{
-		"name": "DaikiMori",
-	})
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	resJson := EnterPUTResBody{
+		Id:   12345,
+		Name: *json.Name,
+	}
+
+	// JSONを返す、200はステータスコード
+	c.JSON(200, resJson)
 }
 
 // structを定義する
-type EnterPOSTReqBody struct {
-	// 必須でないものは、ポインタとして扱う
+type EnterPUTReqBody struct {
+	// 必須でないものは、ポインタとして扱う：何も入ってない時は何も入らない
 	Name *string `json:"name"`
 }
 
-type EnterPOSTResBody struct {
-	Id int `json:"id"`
+type EnterPUTResBody struct {
+	// nameをunknownでも返す
+	// *stringとstringは、型が違うのでだめ。
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
--- app/controllers/Exit/ExitController.go
+++ app/controllers/Exit/ExitController.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 // よりStrictに定義する
 type ExitController struct{}
@@ -8,22 +13,33 @@ type ExitController struct{}
 // httpのリクエストの一連の流れを、Contextが保持する
 func (ExitController) ExitPost(c *gin.Context) {
 
-	c.JSON(200, gin.H{
-		"id":   "12345",
-		"name": "DaikiMori",
-	})
+	json := ExitPUTReqBody{}
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	resJson := ExitPUTResBody{
+		Id:       json.Id,
+		Name:     json.Name,
+		CheckIn:  time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+		CheckOut: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+	}
+
+	c.JSON(200, resJson)
 }
 
 // structを定義する
 type ExitPUTReqBody struct {
-	Id int `json:"id"`
+	Id int `json:"id" binding:"required"`
 	// デフォルトでもunknownが代入される
 	Name string `json:"name"`
 }
 
 type ExitPUTResBody struct {
-	Id       int    `json:"id"`
-	Name     string `json:"name"`
-	CheckIn  string `json:"check_in"`
-	CheckOut string `json:"check_out"`
+	Id       int       `json:"id"`
+	Name     string    `json:"name"`
+	CheckIn  time.Time `json:"check_in"`
+	CheckOut time.Time `json:"check_out"`
 }
